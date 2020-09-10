c3970fd9c9b33d3bba72b8223c4a19730dd6e1a6
ohad <ohad111@gmail.com>
2020-09-10 06:02:40 +0300

    Added styling for the all results section, documentation, cleanup, README.md, testing

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # DMG static website
+
+This project uses gulp as built tool to process sass, minify, copy files and auto refresh the browser window. 
+
+You have two methods of running the application:
+
+  **The easy way**
+  ---
+  Just run the index.html file from the dist folder
+  
+  **The cool (but more involved) method**
+  --
+ install the prerequisites
+   - [nodeJs](https://nodejs.org/en/)
+   - [Sass](https://sass-lang.com/)
+   - [gulp.js](https://gulpjs.com/)
+ 
+ 1. Pull the project `git pull git@github.com:diggerhub/dmg-static.git`
+ 2. Run `npm install`
+ 3. Run `gulp`
+ 
+ A Browser window should open, if it does not then open one and navigate to `localhost:3001`.
+ You can also navigate to the `IP:PORT` of the computer running the script from other computers and mobile devices.
+ 
+ Tested on firefox, chrome and edge.
+ 
+   
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,112 @@
+/**
+ * Checks if an elements contains the css class 'dropDownExpend'
+ * @param element - an HTML element
+ * @returns {boolean}
+ */
+let isDropDownExtended = (element) => element.classList.contains('dropDownExpend');
+/**
+ * Adds the css classes to start the label and icon animations
+ * Removes the transitionend event from the background element
+ * @param event - The animation event
+ * @param element - The drop down element
+ * @param icon - The arrow Icon
+ * @param bgElement - The drop down elements background
+ * @constructor
+ */
+let BgAnimationHandler = (event, element, icon, bgElement) => {
+    element.classList.add('dropDownExpend');
+    icon.classList.add('dropDownRotate');
+    bgElement.removeEventListener('transitionend', BgAnimationHandler);
+};
+/**
+ * Updates the drop down menu's label according to the user selection
+ * @param target - The label element
+ */
+let updateDropDownLabel = target => {
+    let oldLabel = dropDownLabel.innerHTML;
+    dropDownLabel.innerHTML = target.text;
+    for (elm of dropDown.children) {
+        elm.classList.remove('hidden');
+        if (elm.innerText === dropDownLabel.innerHTML) elm.classList.add('hidden');
+    }
+};
+/**
+ * Add or removes the css animation for the border and dropdown animation effect
+ * @param event - The animation effect
+ * @param parent - The element's parent
+ * @param dropDownElement - The dropdown element
+ */
+let dropDownAnimationHandler = (event, parent, dropDownElement) => {
+    for (elm of parent.children) {
+        if (elm.classList.contains('outerBorder')) {
+            elm.classList.remove('outerBorderMoveOut');
+        }
+        if (elm.classList.contains('dropAnimation')) {
+            elm.classList.remove('dropDownMoveIn');
+        }
+    }
+    dropDownElement.removeEventListener('transitionend', dropDownAnimationHandler);
+};
+/**
+ * Add or removes 1 when user clicks on the counter +/- elements
+ * @param Controls - The counter controls
+ * @param target - The counter input element
+ */
+let counterControlsHandler = (Controls, target) => {
+    for (let i = 0; i < 2; i++) {
+        Controls[i].addEventListener('click', event => {
+            if (Controls[i].classList.contains('addIncrement')) {
+                target.value++
+            } else {
+                if (target.value > 1) target.value--
+            }
+        })
+    }
+};
+/**
+ * Handles a users click on the drop down menu
+ * @param parent - The elements parent
+ * @param bgElement - Drop down background
+ * @param dropDownElement - The element itself
+ * @param icon - Arrow icon
+ */
+let dropDownClickHandler = (parent, bgElement, dropDownElement, icon) => {
+    parent.addEventListener('click', event => {
+        if (event.target.tagName === 'A') updateDropDownLabel(event.target);
+        if (!isDropDownExtended(dropDownElement)) {
+            for (elm of parent.children) {
+                if (elm.classList.contains('outerBorder')) {
+                    elm.classList.add('outerBorderMoveOut');
+                }
+                if (elm.classList.contains('dropAnimation')) {
+                    elm.classList.add('dropDownMoveIn');
+                }
+            }
+            bgElement.addEventListener('transitionend', BgAnimationHandler(event, dropDownElement, icon, bgElement))
+        } else {
+            dropDownElement.classList.remove('dropDownExpend');
+            icon.classList.remove('dropDownRotate');
+            dropDownElement.addEventListener('transitionend', dropDownAnimationHandler(event, parent, dropDownElement))
+        }
+    });
+};
+/**
+ * Handles account menu interactions
+ * @param parent - The parent element
+ */
+let accountMenuClickHandler = (parent) => {
+    parent.addEventListener('click', event => {
+        if (parent.children[2].classList.contains('fadeIn')) {
+            parent.children[2].classList.remove('fadeIn')
+        } else {
+            parent.children[2].classList.add('fadeIn')
+        }
+    });
+};
+
+/**
+ * Runs when dom is ready
+ */
 document.addEventListener("DOMContentLoaded", () => {
     const dropDownParent    = document.getElementsByClassName('sortList')[0],
           dropDownIcon      = document.getElementsByClassName('dropDownIcon')[0],
@@ -8,75 +117,7 @@ document.addEventListener("DOMContentLoaded", () => {
           counterTarget     = document.getElementsByClassName('counter')[0],
           counterControls   = document.getElementsByClassName('increments');
 
-
-    let isDropDownExtended = () => dropDown.classList.contains('dropDownExpend');
-    let BgAnimationHandler = event => {
-        dropDown.classList.add('dropDownExpend');
-        dropDownIcon.classList.add('dropDownRotate');
-        dropDownBg.removeEventListener('transitionend', BgAnimationHandler);
-    };
-    let updateDropDownLabel = target => {
-        let oldLabel = dropDownLabel.innerHTML;
-        dropDownLabel.innerHTML = target.text;
-        for (elm of dropDown.children) {
-            elm.classList.remove('hidden');
-            if (elm.innerText === dropDownLabel.innerHTML) elm.classList.add('hidden');
-        }
-    };
-    let dropDownAnimationHandler = event => {
-        for (elm of dropDownParent.children) {
-            if (elm.classList.contains('outerBorder')) {
-                elm.classList.remove('outerBorderMoveOut');
-            }
-            if (elm.classList.contains('dropAnimation')) {
-                elm.classList.remove('dropDownMoveIn');
-            }
-        }
-        dropDown.removeEventListener('transitionend', dropDownAnimationHandler);
-    };
-    let updateTotalprice = (amount, unitPrice) => {
-        console.log(amount, unitPrice);
-    }
-    let counterControlsHandler = () => {
-        for (let i = 0; i < 2; i++) {
-            counterControls[i].addEventListener('click', event => {
-                if (counterControls[i].classList.contains('addIncrement')) {
-                    counterTarget.value++
-                } else {
-                    if (counterTarget.value > 1) counterTarget.value--
-                }
-            })
-        }
-    };
-
-    dropDownParent.addEventListener('click', event => {
-        if (event.target.tagName === 'A') updateDropDownLabel(event.target);
-        if (!isDropDownExtended()) {
-            for (elm of dropDownParent.children) {
-                if (elm.classList.contains('outerBorder')) {
-                    elm.classList.add('outerBorderMoveOut');
-                }
-                if (elm.classList.contains('dropAnimation')) {
-                    elm.classList.add('dropDownMoveIn');
-                }
-            }
-            dropDownBg.addEventListener('transitionend', BgAnimationHandler)
-        } else {
-            dropDown.classList.remove('dropDownExpend');
-            dropDownIcon.classList.remove('dropDownRotate');
-            dropDown.addEventListener('transitionend', dropDownAnimationHandler)
-        }
-    });
-    accountParent.addEventListener('click', event => {
-        if (accountParent.children[2].classList.contains('fadeIn')) {
-            accountParent.children[2].classList.remove('fadeIn')
-        } else {
-            accountParent.children[2].classList.add('fadeIn')
-        }
-    });
-    counterTarget.addEventListener('input', event => {
-        console.log(event.target.value);
-    })
-    counterControlsHandler();
-
+    counterControlsHandler(counterControls, counterTarget);
+    dropDownClickHandler(dropDownParent, dropDownBg, dropDown, dropDownIcon);
+    accountMenuClickHandler(accountParent);
 });
